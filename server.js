0d4b2315e500568ace5a2393259598066898f4c0
theboringschool <theboringschool.org@gmail.com>
2022-10-01 21:44:15 +0545

    Updated

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 // file deepcode ignore UseCsurfForExpress: CSRF Protection will disallow Socket to function properly!
 const app = require("express")();
 const helmet = require("helmet");
-app.use(helmet());
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { socketConfig } = require("./utils/socket");
+const http = require("http").Server(app);
+
 const { dbConfig } = require("./utils/db");
 const APIRouter = require("./utils/router");
-const PORT = process.env.POR || 4000;
-const http = require("http").Server(app);
+const { socketConfig } = require("./utils/socket");
+const PORT = process.env.PORT || 4000;
 
 app
   .use(cors())
   .use(morgan("dev"))
   .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }));
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(helmet());
 
 /**
  * @dev configuration utils
  * 1. Database Configuration
- * 2. Socket Configuration
  */
 dbConfig();
 socketConfig(http);
@@ -37,3 +37,5 @@ app.use("/", APIRouter).get("*", (req, res) =>
 http.listen(PORT, () => {
   console.log(`👾 : Server listening on ${PORT}!`);
 });
+
+module.exports = http;
--- utils/socket.js
+++ utils/socket.js
@@ -1,16 +1,16 @@
 module.exports = {
-    socketConfig: (http) => {
-        const socketIO = require("socket.io")(http, {
-          cors: {
-            origin: "*",
-          },
-        });
-      
-        socketIO.on("connection", (socket) => {
-          console.log(`⚡: ${socket.id} user just connected!`);
-          socket.on("disconnect", () => {
-            console.log("🔥: A user disconnected!");
-          });
-        });
-      }      
-}
+  socketConfig: (http) => {
+    global.socketIO = require("socket.io")(http, {
+      cors: {
+        origin: "*",
+      },
+    });
+
+    socketIO.on("connection", (socket) => {
+      console.log(`⚡: ${socket.id} user just connected!`);
+      socket.on("disconnect", () => {
+        console.log("🔥: A user disconnected!");
+      });
+    });
+  },
+};
